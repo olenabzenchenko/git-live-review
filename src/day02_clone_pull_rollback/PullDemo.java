0d34a7eef24c28c263e43285712fedf552b78ad4
olenabzenchenko <olenazenchenko@gmail.com>
2025-03-16 10:43:12 -0400

    Update PullDemo.java

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -3,6 +3,6 @@ package day02_clone_pull_rollback;
 public class PullDemo {
     public static void main(String[] args) {
         System.out.println("Hello from local");
-        System.out.println("Hello From teammate who pushed to github")
+        System.out.println("Hello From teammate who pushed to github");
     }
 }
