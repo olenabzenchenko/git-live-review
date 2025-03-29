29d2c37826ef69290e066a6ed818193bd1436d52
olenabzenchenko <olenazenchenko@gmail.com>
2025-03-29 12:47:42 -0400

    Update MyComputerObject.java

--- src/day03_branching_pull_request/MyComputerObject.java
+++ src/day03_branching_pull_request/MyComputerObject.java
@@ -4,5 +4,7 @@ public class MyComputerObject {
     public static void main(String[] args) {
         MyComputer myComputer = new MyComputer("Apple MacBook" , 2000.0);
         System.out.println(myComputer);
+        MyComputer myComputer = new MyComputer("Any", 1000.0);
+        
     }
 }
